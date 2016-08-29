5121ff9d90ab72f2563f32a9f3138c8ecc602268
yaoganglong <yaoganglong@qinggukeji.com>
2016-08-29 15:00:21 +0800

    sendcloud webhook email log

--- src/MailHub/Controllers/CallbackController.php
+++ src/MailHub/Controllers/CallbackController.php
@@ -17,15 +17,88 @@ class CallbackController extends Controller implements MailHubLogInterface
     protected $log;
 
     /**
+     * @var http request object
+     */
+    protected $request;
+
+    /**
      * @param EnvironmentManager $environmentManager
      */
-    public function __construct()
+    public function __construct(Request $request)
     {
-        dd($this->test('jump'));
+        $this->request = $request;
     }
 
     public function sendcloud()
     {
+         $options = $this->request->input();
+        
+        // very signature
+        $appkey = $this->getWebhookAppKey();
+        if (!$appkey || !isset($options['signature'])) {
+            return false;
+        }
 
+        // very signature fail
+        if (!$this->verify($appkey, $options['token'], $options['timestamp'], $options['signature'])) {
+            return false;
+        }
+        
+        $handle = [];
+        $data = ['event', 'message', 'apiUser', 'emailId' ,'recipient'];
+
+        foreach ($data as $value) {
+            if (isset($options[$value])) {
+                $handle[$value] = $options[$value];
+            }
+        }
+        if (isset($options['event'])) {
+            switch ($options['event']) {
+                case 'deliver':
+                    $fileSystemName = 'sendcloud_deliver_email';
+                    $logdir = 'getSendCloudLogDir';
+                    break;
+                case 'invalid':
+                    $fileSystemName = 'sendcloud_invalid_email';
+                    $logdir = 'getSendCloudFailLogDir';
+                    break;         
+                default:
+                    $fileSystemName = 'sendcloud_invalid_email';
+                    $logdir = 'getSendCloudFailLogDir';
+                    break;
+            }
+            // writing log
+            $logContent = $this->setlogContent($handle);
+            $fileDir = $this->{$logdir}();
+            $this->setLogName($fileSystemName);
+            $this->logInfo($logContent, $fileDir);
+        }
     }
+
+    /**
+     * very signature
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    private function verify($appkey, $token, $timestamp, $signature)
+    {
+        $hash="sha256";
+        $result=hash_hmac($hash,$timestamp.$token,$appkey);
+        return strcmp($result,$signature)==0?1:0;
+    }
+
+    /**
+     * get sendcloud webhook app_key
+     * @return [bool]          [log config]
+     */
+    public function getWebhookAppKey()
+    {   
+        $appkey = config('mailhub.gateways.sendcloud.webhook_app_key');
+        if (!isset($appkey)) {
+                return $this->throwException('webhook_app_key');
+        }
+        return $appkey;
+    }
+
 }
--- src/MailHub/Traits/MailHubLogTrait.php
+++ src/MailHub/Traits/MailHubLogTrait.php
@@ -1,6 +1,9 @@
 <?php
 namespace MrVokia\MailHub\Traits;
 
+use Storage,
+	Carbon\Carbon;
+use Illuminate\Filesystem\Filesystem;
 /**
  * Here is the mail log module features trait
  * 
@@ -12,14 +15,166 @@ use MrVokia\MailHub\Exception\MailHubException;
 
 trait MailHubLogTrait
 {
+	/**
+     * Log name
+     * @var string
+     */
+    private $logName = 'mail_log.log';
 
+    /**
+     * Log name
+     * @var string
+     */
+    private $local = 'Asia/Shanghai';
 
-    public function test($d)
-    {
-        if ( !isset($d) ) {
-            throw new MailHubException('test');
+    /**
+	 * set log content
+	 */
+	public function setlogContent($handle = array())
+	{
+
+		// local is shanghai
+		$time = Carbon::now($this->local);
+		$logHandle = '['.$time.']';
+		foreach($handle as $key => $val) {
+			$logHandle .= $key.':'.$val.',';
+		}
+		return rtrim($logHandle, ',');
+	}
+
+	/**
+	 * write log
+	 * @param  string $logContent [logContent]
+	 * @param  string $logDirPath [filepath]
+	 */
+	public function logInfo($logContent, $logDirPath)
+	{
+		$filesystem = new Filesystem();
+		if (!$logContent || !$logDirPath) return false;
+		if($this->getmailLog())
+		{
+			// log file all path
+			$logPath = $logDirPath.$this->getLogName();
+			if($filesystem->exists($logPath))
+			{
+				// everyDay new a file
+				$content = $filesystem->get($logPath);
+				if($logTime = substr($content,1,10))
+				{
+					if(Carbon::now($this->local)->toDateString() == $logTime)
+					{
+						$filesystem->append($logPath,$logContent.PHP_EOL);
+					}
+					else
+					{
+						$new_log_path = $logDirPath.$logTime.$this->getLogName();
+						if (!$filesystem->exists($new_log_path)) {
+							$filesystem->move($logPath, $new_log_path);
+						}
+							$filesystem->put($logPath,$logContent);
+					}
+				}
+        	}
+        	else
+        	{
+            	$filesystem->put($logPath,$logContent);
+        	}
         }
-        return $d . ' - log';
+    	
+	}
+
+	/**
+	 * get log config
+	 * @return [bool]          [log config]
+	 */
+	public function getmailLog()
+	{	
+		$mailLog = config('mailhub.mail_log');
+		if (!isset($mailLog)) {
+                return $this->throwException('mail_log');
+        }
+		return $mailLog;
+	}
+
+	/**
+	 * get sendcloud log dir
+	 * @return [string]   	    [logpath]
+	 */
+	public function getSendCloudLogDir()
+	{	
+		$dir = config('mailhub.sendcloud_log_dir');
+		if (!isset($dir)) {
+                return $this->throwException('sendcloud_log_dir');
+        }
+		return $dir;
+	}
+
+	/**
+	 * get sendcloud fail log dir
+	 * @return [string]   	    [logpath]
+	 */
+	public function getSendCloudFailLogDir()
+	{	
+		$dir = config('mailhub.sendcloud_fail_log_dir');
+		if (!isset($dir)) {
+                return $this->throwException('sendcloud_fail_log_dir');
+        }
+		return $dir;
+	}
+
+	/**
+	 * get Swiftmail log dir
+	 * @return [string]   	    [logpath]
+	 */
+	public function getSwiftmailLogDir()
+	{	
+		$dir = config('mailhub.swiftmail_log_dir');
+		if (!isset($dir)) {
+                return $this->throwException('swiftmail_log_dir');
+        }
+		return $dir;
+	}
+
+	/**
+	 * get Swiftmail fail log dir
+	 * @return [string]   	    [logpath]
+	 */
+	public function getSwiftmailFailLogDir()
+	{	
+		$dir = config('mailhub.swiftmail_fail_log_dir');
+		if (!isset($dir)) {
+                return $this->throwException('swiftmail_fail_log_dir');
+        }
+		return $dir;
+	}
+
+	/**
+	 * set log name
+	 * @param [string]  $logName    [log name]
+	 */
+	public function setLogName($logName = 'mail_log')
+	{	
+		$this->logName = $logName.'.log';
+	}
+
+	/**
+	 * get log name
+	 * @param [string]  $logName    [log name]
+	 */
+	public function getLogName()
+	{	
+		return $this->logName;
+	}
+
+	/**
+     * Throw an exception
+     * @param  string $msg Exception message
+     * @return MrVokia\MailHub\Exception\MailHubException
+     */
+    public function throwException($msg)
+    {
+        $msg = 'Not Found Config From ' . $msg;
+        throw new MailHubException($msg);
     }
 
 }
